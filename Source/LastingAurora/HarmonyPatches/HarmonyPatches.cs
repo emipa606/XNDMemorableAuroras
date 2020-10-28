762d94c15c661eba0564942e54234bbdde3e7d55
emipa606 <emil.offermo@litet.net>
2020-10-28 10:30:03 +0100

    Updated to 1.2

--- Source/LastingAurora/HarmonyPatches/HarmonyPatches.cs
+++ Source/LastingAurora/HarmonyPatches/HarmonyPatches.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Verse;
-using RimWorld;
-using HarmonyLib;
+﻿using Verse;
 
 namespace MemorableAuroras
 {
