ec72bfc9e542f4d12fa1a98e9af0d5f751be9226
Mlie <emipa606@users.noreply.github.com>
2025-06-25 19:46:23 +0200

    Mod updated for 1.6 and passed autotests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# XNDMemorableAuroras
+# [[XND] Memorable Auroras (Continued)](https://steamcommunity.com/sharedfiles/filedetails/?id=2270039395)
 
 ![Image](https://i.imgur.com/buuPQel.png)
 
@@ -8,7 +8,6 @@ https://steamcommunity.com/sharedfiles/filedetails/?id=1437612522
 - Added french translation, thanks qux!
 
 ![Image](https://i.imgur.com/pufA0kM.png)
-
 	
 ![Image](https://i.imgur.com/Z4GOv8H.png)
 
@@ -44,13 +43,12 @@ Should be safely addable to existing savegames. Can be removed from existing sav
 
 -  See if the the error persists if you just have this mod and its requirements active.
 -  If not, try adding your other mods until it happens again.
--  Post your error-log using https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib or the standalone https://steamcommunity.com/sharedfiles/filedetails/?id=2873415404]Uploader and command Ctrl+F12
+-  Post your error-log using [HugsLib](https://steamcommunity.com/workshop/filedetails/?id=818773962) or the standalone [Uploader](https://steamcommunity.com/sharedfiles/filedetails/?id=2873415404) and command Ctrl+F12
 -  For best support, please use the Discord-channel for error-reporting.
 -  Do not report errors by making a discussion-thread, I get no notification of that.
 -  If you have the solution for a problem, please post it to the GitHub repository.
--  Use https://github.com/RimSort/RimSort/releases/latest]RimSort to sort your mods
+-  Use [RimSort](https://github.com/RimSort/RimSort/releases/latest) to sort your mods
 
+ 
 
-
-https://steamcommunity.com/sharedfiles/filedetails/changelog/2270039395]![Image](https://img.shields.io/github/v/release/emipa606/XNDMemorableAuroras?label=latest%20version&style=plastic&color=9f1111&labelColor=black)
-
+[![Image](https://img.shields.io/github/v/release/emipa606/XNDMemorableAuroras?label=latest%20version&style=plastic&color=9f1111&labelColor=black)](https://steamcommunity.com/sharedfiles/filedetails/changelog/2270039395) | tags:  mood,  lasting
--- Source/LastingAurora/HarmonyPatches/Patch_GameCondition_Aurora.cs
+++ Source/LastingAurora/HarmonyPatches/Patch_GameCondition_Aurora.cs
@@ -4,30 +4,27 @@ using Verse;
 
 namespace MemorableAuroras;
 
-public static class Patch_GameCondition_Aurora
+[HarmonyPatch(typeof(GameCondition_Aurora), nameof(GameCondition_Aurora.GameConditionTick))]
+public static class GameCondition_Aurora_GameConditionTick
 {
-    [HarmonyPatch(typeof(GameCondition_Aurora), nameof(GameCondition_Aurora.GameConditionTick))]
-    public static class GameConditionTick
+    public static void Postfix(GameCondition_Aurora __instance)
     {
-        public static void Postfix(GameCondition_Aurora __instance)
+        // Try to add 'Observed aurora' to all pawns every tick rare interval
+        if (GenTicks.TicksGame % GenTicks.TickRareInterval != 0)
         {
-            // Try to add 'Observed aurora' to all pawns every tick rare interval
-            if (GenTicks.TicksGame % GenTicks.TickRareInterval != 0)
-            {
-                return;
-            }
+            return;
+        }
 
-            foreach (var curMap in __instance.AffectedMaps)
+        foreach (var curMap in __instance.AffectedMaps)
+        {
+            for (var j = 0; j < curMap.mapPawns.AllPawnsSpawnedCount; j++)
             {
-                for (var j = 0; j < curMap.mapPawns.AllPawnsSpawnedCount; j++)
+                var pawn = curMap.mapPawns.AllPawnsSpawned[j];
+                if (pawn.needs.mood != null && !pawn.PositionHeld.Roofed(curMap) &&
+                    pawn.health.capacities.CapableOf(PawnCapacityDefOf.Sight))
                 {
-                    var pawn = curMap.mapPawns.AllPawnsSpawned[j];
-                    if (pawn.needs.mood != null && !pawn.PositionHeld.Roofed(curMap) &&
-                        pawn.health.capacities.CapableOf(PawnCapacityDefOf.Sight))
-                    {
-                        pawn.needs.mood.thoughts.memories.TryGainMemory(
-                            (Thought_Memory)ThoughtMaker.MakeThought(MA_ThoughtDefOf.ObservedAurora));
-                    }
+                    pawn.needs.mood.thoughts.memories.TryGainMemory(
+                        (Thought_Memory)ThoughtMaker.MakeThought(MA_ThoughtDefOf.ObservedAurora));
                 }
             }
         }
